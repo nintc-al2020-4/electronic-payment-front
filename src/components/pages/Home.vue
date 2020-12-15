00c5d5745af7dbd4261bdaf498507ea6e7ef08ef
Crow314 <master@crow31415.net>
2020-12-15 15:04:07 +0900

    Set routing

--- src/components/pages/Home.vue
+++ src/components/pages/Home.vue
@@ -1,5 +1,5 @@
 <template>
-
+<p>hello</p>
 
 </template>
 
--- src/main.js
+++ src/main.js
@@ -1,4 +1,41 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+// Routing
+
+import { createRouter, createWebHistory } from 'vue-router'
+
+import Home from "./components/pages/Home";
+import Charge from "./components/pages/Charge";
+import Pay from "./components/pages/Pay";
+import Wallet from "./components/pages/Wallet";
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [{
+            path: '/',
+            name: 'home',
+            component: Home
+        }, {
+            path: '/charge',
+            name: 'charge',
+            component: Charge
+        }, {
+            path: '/pay',
+            name: 'pay',
+            component: Pay
+        }, {
+            path: '/wallet',
+            name: 'wallet',
+            component: Wallet
+        }, {
+            path: '/:pathMatch(.*)*',
+            name: 'NotFound'
+        }
+    ]
+})
+
+app.use(router)
+app.mount('#app')

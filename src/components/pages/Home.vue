bb9bfa6791b8f7147703f4f3ac6cbeab58dc3ff2
Crow314 <master@crow31415.net>
2020-12-16 23:46:50 +0900

    Update Home.vue

--- src/components/pages/Home.vue
+++ src/components/pages/Home.vue
@@ -1,6 +1,27 @@
 <template>
-<p>hello</p>
-
+  <div id="home">
+    <div class="container">
+      <a href="/wallet" class="card">
+        <div class="card-body">
+          <p class="card-text">
+            残高を見る
+          </p>
+        </div>
+      </a>
+      <div class="row">
+        <div class="col-md-6 col-12">
+          <a href="/charge" class="btn btn-dark text-light w-100 my-2" role="button">
+            チャージする
+          </a>
+        </div>
+        <div class="col-md-6 col-12">
+          <a href="pay" class="btn btn-dark text-light w-100 my-2" role="button">
+            支払う(QRコード表示)
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>

3c1e0640fb9ee2e0243ae24079f8ae3df62d30a2
Crow314 <master@crow31415.net>
2020-12-16 11:44:24 +0900

    Update Charge.vue

--- src/components/pages/Charge.vue
+++ src/components/pages/Charge.vue
@@ -1,54 +1,49 @@
 <template>
-<title>Charge</title>
   <div id="Charge">
-  <div class="container-fluid">
-    <table class="table table-bordered">
-      <colgroup>
-        <col width="50%">
-        <col width="50%">
-      </colgroup>
-      <thead>
-        <tr>
-          <th>入金額</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>
-            <div class="form-group mb-0">
-              <label>入金額を入力してください</label>
-              <input v-model.number="number" type="int" class="form-control">
-              <p v-show="NotNumber" v-bind:style="styleObject">入力が数値ではありません</p>
-            </div>
-          </td>
-          
-        </tr>
-      </tbody>
-    </table>
-  </div>
-    <button type="submit">チャージ</button>
-</div>
+    <div class="container-fluid">
+      <table class="table table-bordered">
+        <colgroup>
+          <col width="50%">
+          <col width="50%">
+        </colgroup>
+        <thead>
+          <tr>
+            <th>入金額</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <div class="form-group mb-0">
+                <label>入金額を入力してください
+                  <input v-model.number="number" type="number" class="form-control" min="0">
+                </label>
+                <p v-show="NotNumber" v-bind:style="styleObject">入力が数値ではありません</p>
+              </div>
+            </td>
 
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-<script src="https://unpkg.com/vue"></script>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+      <button type="submit">チャージ</button>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "Charge"
-}
+  name: "Charge",
 
-var app = new Vue({
-  el: '#Charge',
-  data: {
-    number: null,
-    styleObject: {
-       color: 'red'
+  data() {
+    return {
+      number: null,
+      styleObject: {
+        color: 'red'
+      }
     }
   },
   methods: {
-    onSubmit: function () {
+    onSubmit() {
       // 実際はここでWeb APIを叩いて
       // フォームの内容をサーバに送信するはず
     }
@@ -60,7 +55,7 @@ var app = new Vue({
       return Number.isNaN(value);
     }
   }
-})
+}
 </script>
 
 <style scoped>

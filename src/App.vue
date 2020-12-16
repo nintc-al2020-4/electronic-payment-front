40b25ed21481299ffe4106f476ec1dd151ca3f1d
Crow314 <master@crow31415.net>
2020-12-16 23:45:40 +0900

    Set page title

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,36 @@
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  methods : {
+    setTitleDescription(routeInstance) {
+      // タイトルを設定
+      const defaultTitle = "奈良高専電子決済";
+      const title = routeInstance.meta.title
+      if (title) {
+        document.title = title + ' - ' + defaultTitle;
+      } else {
+        document.title = defaultTitle;
+      }
+
+      // メタタグdescription設定
+      const desc = routeInstance.meta.description
+      if (desc) {
+        document.querySelector("meta[name='description']").setAttribute('content', desc)
+      } else {
+        document.querySelector("meta[name='description']").setAttribute('content', 'description is not set')
+      }
+    }
+  },
+  mounted(){
+    const routeInstance = this.$route;
+    this.setTitleDescription(routeInstance);
+  },
+  watch: {
+    '$route' (routeInstance) {
+      this.setTitleDescription(routeInstance);
+    }
+  }
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -7,29 +7,33 @@ const app = createApp(App)
 
 import { createRouter, createWebHistory } from 'vue-router'
 
-import Home from "./components/pages/Home";
-import Charge from "./components/pages/Charge";
-import Pay from "./components/pages/Pay";
-import Wallet from "./components/pages/Wallet";
+import Home from "@/components/pages/Home";
+import Charge from "@/components/pages/Charge";
+import Pay from "@/components/pages/Pay";
+import Wallet from "@/components/pages/Wallet";
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [{
             path: '/',
             name: 'home',
-            component: Home
+            component: Home,
+            meta: {title: "トップページ", description: ""}
         }, {
             path: '/charge',
             name: 'charge',
-            component: Charge
+            component: Charge,
+            meta: {title: "チャージ", description: ""}
         }, {
             path: '/pay',
             name: 'pay',
-            component: Pay
+            component: Pay,
+            meta: {title: "お支払い", description: ""}
         }, {
             path: '/wallet',
             name: 'wallet',
-            component: Wallet
+            component: Wallet,
+            meta: {title: "残高", description: ""}
         }, {
             path: '/:pathMatch(.*)*',
             name: 'NotFound'

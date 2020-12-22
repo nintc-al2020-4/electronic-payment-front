3adb8f88c713f8a8c16700516ff1893425e59acc
mukumotoJun <mjun926@gmail.com>
2020-12-22 12:07:03 +0900

    PayをPay_Qrに対応

--- src/components/pages/Pay.vue
+++ src/components/pages/Pay.vue
@@ -1,14 +1,17 @@
 <template>
- 以下のQRコードをスキャンしてください<br>
- <img src="@/assets/payment_qr_code.png"><br>
+ <h4>以下のQRコードをスキャンしてください</h4>
+ <!--QRコードは仮です。リポジトリのトップのQRコードです。-->
+ <PayQR></PayQR><br>
  <a href="/" class="btn btn-dark" role="button">戻る</a>
 </template>
 
 <script>
+import PayQR from '@/components/parts/Pay_Qr.vue'
+
 export default {
   name: "Pay",
   components: {
-    
+    PayQR
   },
   methods: {
     
